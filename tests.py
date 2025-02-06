e2f7ef1a369c2a78b73061415ca4bd12488759aa
Alina Sitalova <alinasitalova@yandex.ru>
2025-02-06 11:58:02 +0300

    add confest.py

--- tests.py
+++ tests.py
@@ -1,34 +1,4 @@
 import pytest
-from main import BooksCollector
-
-@pytest.fixture
-def collector():
-    return BooksCollector()
-@pytest.fixture
-def books():
-    books = BooksCollector()
-    books.books_genre = {
-        'Игра Эндера': 'Фантастика',
-        'Интерстеллар': 'Фантастика',
-        'Кто похитил мой мозг?': 'Детективы',
-        'Смешарики': 'Мультфильмы',
-        'Веселые истории': 'Комедии'}
-    return books
-
-@pytest.fixture
-def favorite_books():
-    favorite_books = BooksCollector()
-    favorite_books.books_genre = {
-        'Игра Эндера': 'Фантастика',
-        'Интерстеллар': 'Фантастика',
-        'Кто похитил мой мозг?': 'Детективы',
-        'Смешарики': 'Мультфильмы',
-        'Веселые истории': 'Комедии'}
-    favorite_books.favorites = [
-        'Смешарики',
-        'Веселые истории'
-    ]
-    return favorite_books
 
 class TestBooksCollector:
     @pytest.mark.parametrize('name',
